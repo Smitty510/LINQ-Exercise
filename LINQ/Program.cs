3f4798c606b900e1bb23645c591454f3ce9b0f63
Patrick Smith <Patrick_Gregory_Smith@proton.me>
2023-07-10 16:41:58 -0400

    LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace LINQ
+﻿class Program
 {
-    public class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        List<string> games = new List<string>
         {
-            Console.WriteLine("Hello, World!");
+            "The Legend of Zelda: Breath of the Wild",
+            "Final Fantasy VII Remake",
+            "Red Dead Redemption 2",
+            "God of War",
+            "Minecraft",
+            "Super Mario Odyssey",
+            "Call of Duty: Modern Warfare",
+            "Fortnite",
+            "Overwatch"
+        };
+
+
+        var orderedGames = games.OrderBy(game => game.Length);
+
+
+        Console.WriteLine("Ordered Game Names:");
+        foreach (var game in orderedGames)
+        {
+            Console.WriteLine(game);
         }
     }
 }
